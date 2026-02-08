f3fb94b303de01c351ddff1766f6da2efed8686e
Maity <sahan.maity@siemens.com>
2026-02-08 12:16:17 +0530

    implemented subjectsRouter and fetch

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import subjectsRouter from './routes/subjects'
 
 const app = express()
-const router = express.Router()
 app.use(express.json())
-app.use("/api", router)
+app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
     res.send("Hello World")
